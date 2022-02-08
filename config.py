f6a29b41a7f13009c497e6febb52632cb861906b
Jlove <j.love36314@gmail.com>
2022-02-08 17:46:25 -0500

    m

---
+++ config.py
@@ -0,0 +1,1 @@
+weather_api_key="feceabd5a6a649ad82a3041ff9181356"
---
+++ config.py.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+weather_api_key="feceabd5a6a649ad82a3041ff9181356"
+
+
+# In[ ]:
+
+
+
+
